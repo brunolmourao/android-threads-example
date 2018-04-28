54621f088b86f175f593772e14f709651b7cd44a
Bruno Mourao <brunolmourao10@gmail.com>
2018-04-28 17:55:00 -0300

    Funcionando

--- app/src/main/java/comt/example/bruno/threadexemple/MainActivity.java
+++ app/src/main/java/comt/example/bruno/threadexemple/MainActivity.java
@@ -1,20 +1,27 @@
 package comt.example.bruno.threadexemple;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.ref.WeakReference;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     ImageView image;
     Button botao;
     ProgressBar pb;
-    WeakReference weakReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,18 +30,42 @@ public class MainActivity extends AppCompatActivity {
         botao = findViewById(R.id.button);
         image = findViewById(R.id.imageView);
         pb = findViewById(R.id.progressBar);
-        weakReference = new WeakReference(image);
 
         botao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                chamarAsyncTask("https://upload.wikimedia.org/wikipedia/pt/3/3b/Dark_Side_of_the_Moon.png",weakReference);
+                switch(v.getId()){
+                    case R.id.button:
+                        pb.setVisibility(View.VISIBLE);
+                        LoadImage loadImage = new LoadImage();
+                        loadImage.execute("https://upload.wikimedia.org/wikipedia/pt/3/3b/Dark_Side_of_the_Moon.png");
+                        break;
+                }
             }
         });
     }
-    private void chamarAsyncTask(String url,WeakReference<ImageView> weakReference){
-        LoadImage ld = new LoadImage(weakReference);
-        ld.execute(url);
-    }
+    private class LoadImage extends AsyncTask<String, Void, Bitmap> {
 
+        @Override
+        protected Bitmap doInBackground(String[] params) {
+            try {
+                return DownloadImage.baixarImagem(params[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            image.setImageBitmap(result);
+            pb.setVisibility(View.INVISIBLE);
+            Log.v("tag","Rodando após o back");
+        }
+
+    }
 }
+
+
+
+
